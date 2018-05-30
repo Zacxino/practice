a3fc25b785b729590cd0490e5a6bd126ae8dcf36
Zacxino <scarllch@163.com>
2018-05-30 09:32:51 +0800

    new repository

---
+++ 林业系统/Data/DataExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Forest.Data;
+
+namespace Unity
+{
+    public static class DataExtension
+    {
+        public static IUnityContainer AddFCMSData(this IUnityContainer container)
+        {
+            container.RegisterType<ForestDbContext, ForestDbContext>();
+            return container;
+        }
+    }
+}
